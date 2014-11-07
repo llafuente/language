725475d44e90cec50d69ba80fe007c41d309cd9b
llafuente <llafuente@noboxout.com>
2014-11-07 14:22:12 +0100

    optional parenthesis and mandatory braces

--- create_test.js
+++ create_test.js
@@ -3,7 +3,7 @@
 var grammar = require("./plee-grammar.js"),
   fs = require("fs"),
   util = require("util"),
-  js_beautify = require('js-beautify');
+  beautify = require('js-beautify').js_beautify;
 
 if (process.argv.length == 2) {
     throw new Error("parse file not sent");
@@ -13,17 +13,12 @@ var contents = fs.readFileSync(process.argv[2], 'utf-8'),
     result;
 
 try {
-  result = require("util").inspect(
-    grammar.parse(contents), {colors: true, depth: null}
-  );
+  result = grammar.parse(contents);
+  //require("util").inspect(result, {colors: true, depth: null});
 
 } catch(e) {
     result = e;
 }
 
 
-var opts = jsbeautifier.default_options()
-opts.indent_size = 2
-var res = jsbeautifier.beautify(JSON.strinfigy(result), opts);
-
-console.log(res);
+console.log(JSON.stringify(result, null, 2));
