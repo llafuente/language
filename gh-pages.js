a1a0543155813c334d52d2ff887a4bb3309a0002
llafuente <llafuente@noboxout.com>
2014-11-14 11:49:52 +0100

    gh-pages generation, manual

--- gh-pages.js
+++ gh-pages.js
@@ -1,9 +1,64 @@
 var fs = require("fs"),
-  marked = require('marked');
+  path = require('path'),
+  marked = require('marked'),
+  mktemp = require('mktemp'),
+  tmp = mktemp.createDirSync('XXXXX.tmp');
 
-var readme = fs.readFileSync("readme.markdown", "utf-8");
-marked(readme, function(err, content) {
+console.log("tmp-dir", tmp);
+
+var files = {
+
+    "readme.markdown": "index.html",
+
+    "types/_intro.markdown": "types.html",
+    "types/string.markdown": "string.html",
+    "types/numerics.markdown": "numerics.html",
+    "types/array.markdown": "array.html",
+    "types/iterable.markdown": "iterable.html",
+    "types/observers.markdown": "observers.html",
+
+    "functions/_intro.markdown": "functions.html",
+    "functions/listeners.markdown": "functions-listeners.html",
+
+    "statements/for.markdown": "for-statement.html",
+    "statements/if.markdown": "if-statement.html",
+    "statements/swap.markdown": "swap-statement.html",
+    "statements/switch.markdown": "switch-statement.html",
+
+    "testing/testing.markdown": "testing.html",
+
+};
+/* multi-file
+var tpl = fs.readFileSync("./tpl.html", "utf-8");
+Object.keys(files).forEach(function(source_file) {
+
+    var source = fs.readFileSync(path.join(__dirname, source_file), "utf-8");
+
+    marked(source, function(err, content) {
+      content = tpl.replace("%body%", content);
+
+      console.log(err);
+
+      fs.writeFileSync(path.join(tmp, files[source_file]), content);
+    });
+});
+*/
+
+/* single-file*/
+var tpl = fs.readFileSync("./tpl.html", "utf-8"),
+    source = "";
+
+Object.keys(files).forEach(function(source_file) {
+
+    source += fs.readFileSync(path.join(__dirname, source_file), "utf-8");
+
+});
+
+marked(source, function(err, content) {
+  content = tpl.replace("%body%", content);
+
   console.log(err);
-  console.log(content);
+
+  fs.writeFileSync(path.join(tmp, "index.html"), content);
 });
 
