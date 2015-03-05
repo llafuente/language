8f8ec0a52fe11a3cb06f4fb5661482c0bf4411dd
llafuente <llafuente@noboxout.com>
2015-03-05 18:25:26 +0000

    pointers ir test

--- playground/c/pointers.c
+++ playground/c/pointers.c
@@ -1,17 +1,54 @@
 #include <stdlib.h>
 
-char * c;
+#define TYPE int
+TYPE *c;
 
 int main() {
+int i = 50;
 _malloc:
-  c = (char*) malloc(10);
+  c = (TYPE*) malloc(i);
 
 raw_pointer_assignament:
   *c = 'Z';
+
 index_assignament:
-  c[1] = 'W';
+  c[99] = 'W';
+
+  free(c);
+
 _return:
   return 0;
 }
 
-//clang -O0 -emit-llvm -mllvm -debug-pass=Structure -S -c -o /dev/stdout
+//clang -Os -O0 -emit-llvm -mllvm -debug-pass=Structure -S -c -o /dev/stdout
+
+/*
+load
+  {++position} = load {subtype}** {variable|++position}, align 8
+
+store
+  store {subtype}* {variable|++position}, {subtype}** {variable|++position}, align 8
+
+move
+  %8 = getelementptr inbounds {subtype}* %7, i64 {position}
+
+allocate
+  %3 = call noalias {subtype}* @malloc(i64 {size}) #2
+  store {subtype}* %3, {subtype}** @{variable}, align 8
+
+deallocate
+  call void @free({subtype}* %9) #2
+
+assign 0
+  %5 = load {subtype}** @{variable}, align 8
+  store {subtype} {store_value}, {subtype}* %5, align 1
+  br label %6
+
+assign 1
+  %7 = load {subtype}** @{variable}, align 8
+  %8 = getelementptr inbounds {subtype}* %7, i64 {position}
+  store {subtype} {store_value}, {subtype}* %8, align 1
+  br label %9
+
+
+*/
