5cabdca233e0297a39964d562bc860288ad535a2
Luis Lafuente <luis.lafuente@inetsys.es>
2015-03-05 13:46:58 +0100

    tests and fixes parsing functions

--- src-generators/test.sh
+++ src-generators/test.sh
@@ -3,6 +3,10 @@
 sh generate.sh
 
 llvm-link -S -o math-test-linked.ll ../src/math.ll test.ll
-LD_PRELOAD=openlibm/libopenlibm.so lli math-test-linked.ll
+LD_PRELOAD="openlibm/libopenlibm.so" lli math-test-linked.ll
+#gcc -lpcre2-8 -llibopenlibm
 
 # output should be 2.00000 :)
+
+clang -S -emit-llvm -O0 pcre.c -o pcre.ll
+LD_PRELOAD="openlibm/libopenlibm.so pcre2/.libs/libpcre2-8.so" lli pcre.ll
---
+++ tests/test-functions.js
@@ -0,0 +1,373 @@
+require("ass");
+
+var tap = require("tap"),
+test = tap.test,
+tokenizer = require("../lib/tokenizer.js"),
+parser = require("../lib/parser.js");
+
+function inspect(obj) {
+  console.log(
+    require("util").inspect(obj, {depth:null, colors: true})
+  );
+}
+
+function setup(code) {
+  var tokens = tokenizer.parse(code, 0);
+  tokens = tokenizer.rangenize (tokens);
+  try {
+    var ast = parser(tokens, 0);
+  } catch(e) {
+    return e;
+  }
+
+  return ast;
+}
+
+test("anonymous function + comma parameters", function (t) {
+  var ast = setup("fn , a {}\n");
+
+  t.ok(!(ast instanceof Error), "no error");
+
+  t.deepEqual(ast.body[0], {
+    "type" : "function-declaration",
+    "range" : [0,8],
+    "id" : null,
+    "parameters" : {
+      "type" : "parameters",
+      "range" : [4,6],
+      "list" : [{
+        "type" : "parameter",
+        "range" : [4,6],
+        "clone" : false,
+        "const" : false,
+        "id" : {
+          "type" : "var-literal",
+          "range" : [4,6],
+          "value" : "a"
+        },
+        "default" : null,
+        "assertions" : null
+      }]
+    },
+    "return_type" : null,
+    "body" : {
+      "type" : "block",
+      "range" : [6,8],
+      "body" : null
+    }
+
+  });
+
+  t.end();
+});
+
+test("anonymous function no parameters", function (t) {
+  var ast = setup("fn {}");
+
+  t.ok(!(ast instanceof Error), "no error");
+
+  t.deepEqual(ast.body[0], {
+    "type" : "function-declaration",
+    "range" : [0,4],
+    "id" : null,
+    "parameters" : {
+      "type" : "parameters",
+      "range" : [2,2],
+      "list" : []
+    },
+    "return_type" : null,
+    "body" : {
+      "type" : "block",
+      "range" : [2,4],
+      "body" : null
+    }
+
+  });
+
+  t.end();
+});
+
+test("anonymous function + parenthesis parameters", function (t) {
+  var ast = setup("fn (a) {}");
+
+  t.ok(!(ast instanceof Error), "no error");
+
+  t.deepEqual(ast.body[0], {
+    "type" : "function-declaration",
+    "range" : [0,8],
+    "id" : null,
+    "parameters" : {
+      "type" : "parameters",
+      "range" : [3,4],
+      "list" : [{
+        "type" : "parameter",
+        "range" : [3,4],
+        "clone" : false,
+        "const" : false,
+        "id" : {
+          "type" : "var-literal",
+          "range" : [3,4],
+          "value" : "a"
+        },
+        "default" : null,
+        "assertions" : null
+      }]
+    },
+    "return_type" : null,
+    "body" : {
+      "type" : "block",
+      "range" : [6,8],
+      "body" : null
+    }
+
+  });
+
+  t.end();
+});
+
+
+
+test("named function no parameters", function (t) {
+
+  var ast = setup("fn fn_name {}");
+
+  t.ok(!(ast instanceof Error), "no error");
+
+  t.deepEqual(ast.body[0], {
+    "type" : "function-declaration",
+    "range" : [0,6],
+    "id" : {
+      "type" : "var-literal",
+      "range" : [2,4],
+      "value" : "fn_name"
+    },
+    "parameters" : {
+      "type" : "parameters",
+      "range" : [4,4],
+      "list" : []
+    },
+    "return_type" : null,
+    "body" : {
+      "type" : "block",
+      "range" : [4,6],
+      "body" : null
+    }
+
+  });
+
+  t.end();
+});
+
+
+test("named function with complex parameters", function (t) {
+
+  var ast = setup("fn fn_name const clone ui8 a, clone ui8{} b, const f32[5][] c {}");
+console.log(ast);
+console.log(ast.stack);
+  t.ok(!(ast instanceof Error), "no error");
+
+  t.deepEqual(ast.body[0], {
+    "type" : "function-declaration",
+    "range" : [0,35],
+    "id" : {
+      "type" : "var-literal",
+      "range" : [2,4],
+      "value" : "fn_name"
+    },
+    "parameters" : {
+      "type" : "parameters",
+      "range" : [4,33],
+      "list" : [{
+        "type" : "parameter",
+        "range" : [4,11],
+        "clone" : true,
+        "const" : true,
+        "var_type" : {
+          "type" : "ui8",
+          "range" : [8,10],
+          "sub_type" : null,
+          "dimensions" : null
+        },
+        "id" : {
+          "type" : "var-literal",
+          "range" : [10,11],
+          "value" : "a"
+        },
+        "default" : null,
+        "assertions" : null
+      },{
+        "type" : "parameter",
+        "range" : [13,20],
+        "clone" : true,
+        "const" : false,
+        "var_type" : {
+          "type" : "object",
+          "range" : [15,19],
+          "sub_type" : null,
+          "dimensions" : null
+        },
+        "id" : {
+          "type" : "var-literal",
+          "range" : [19,20],
+          "value" : "b"
+        },
+        "default" : null,
+        "assertions" : null
+      },{
+        "type" : "parameter",
+        "range" : [22,33],
+        "clone" : false,
+        "const" : true,
+        "var_type" : {
+          "type" : "array",
+          "range" : [25,31],
+          "sub_type" : {
+            "type" : "f32",
+            "range" : [24,25],
+            "sub_type" : null,
+            "dimensions" : null
+          },
+          "dimensions" : [{
+            "type" : "number-literal",
+            "range" : [26,27],
+            "value" : 5
+          },{
+            "type" : "number-literal",
+            "value" : 0
+          }]
+        },
+        "id" : {
+          "type" : "var-literal",
+          "range" : [31,33],
+          "value" : "c"
+        },
+        "default" : null,
+        "assertions" : null
+      }]
+    },
+    "return_type" : null,
+    "body" : {
+      "type" : "block",
+      "range" : [33,35],
+      "body" : null
+    }
+
+  });
+
+  t.end();
+});
+
+
+
+test("default paramaters", function (t) {
+  var ast = setup("fn sum a, b = 0 {}\n");
+
+  t.ok(!(ast instanceof Error), "no error");
+
+  t.deepEqual(ast.body[0].parameters.list,       [{
+    "type" : "parameter",
+    "range" : [4,5],
+    "clone" : false,
+    "const" : false,
+    "id" : {
+      "type" : "var-literal",
+      "range" : [4,5],
+      "value" : "a"
+    },
+    "default" : null,
+    "assertions" : null
+  },{
+    "type" : "parameter",
+    "range" : [7,13],
+    "clone" : false,
+    "const" : false,
+    "id" : {
+      "type" : "var-literal",
+      "range" : [7,9],
+      "value" : "b"
+    },
+    "default" : {
+      "type" : "number-literal",
+      "range" : [11,13],
+      "value" : 0
+    },
+    "assertions" : null
+  }]);
+
+  t.end();
+});
+
+
+test("default paramaters 2", function (t) {
+  var ast = setup("fn test struct a, b = a.length != 0 != null {}\n");
+
+  t.ok(!(ast instanceof Error), "no error");
+
+  t.deepEqual(ast.body[0].parameters.list,[
+    {
+      "type" : "parameter",
+      "range" : [4,7],
+      "clone" : false,
+      "const" : false,
+      "var_type" : {
+        "type" : "struct",
+        "range" : [4,6],
+        "sub_type" : null,
+        "dimensions" : null
+      },
+      "id" : {
+        "type" : "var-literal",
+        "range" : [6,7],
+        "value" : "a"
+      },
+      "default" : null,
+      "assertions" : null
+    },
+    {
+      "type" : "parameter",
+      "range" : [9,26],
+      "clone" : false,
+      "const" : false,
+      "id" : {
+        "type" : "var-literal",
+        "range" : [9,11],
+        "value" : "b"
+      },
+      "default" : {
+        "type" : "member-expr",
+        "range" : [13,17],
+        "object" : {
+          "type" : "var-literal",
+          "range" : [13,14],
+          "value" : "a"
+        },
+        "properties" : [{
+          "type" : "string-literal",
+          "range" : [15,17],
+          "value" : "length"
+        }]
+      },
+      "assertions" : [{
+        "type" : "parameter-assertion",
+        "range" : [17,21],
+        "operator" : "!=",
+        "value" : {
+          "type" : "number-literal",
+          "range" : [19,21],
+          "value" : 0
+        }
+      },{
+        "type" : "parameter-assertion",
+        "range" : [21,26],
+        "operator" : "!=",
+        "value" : {
+          "type" : "null-literal",
+          "range" : [23,26]
+        }
+      }]
+    }
+
+  ]);
+
+  t.end();
+});
